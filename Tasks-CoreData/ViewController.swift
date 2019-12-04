dd9d10760bcf54fe3302454017dbbbe3eb275711
Christian Lorenzo <clbmiami2004@gmail.com>
2019-12-03 18:46:03 -0800

    Initial Commit

---
+++ Tasks-CoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tasks-CoreData
+//
+//  Created by Lambda_School_Loaner_201 on 12/3/19.
+//  Copyright © 2019 Christian Lorenzo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
