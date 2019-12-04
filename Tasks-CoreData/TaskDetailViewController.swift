a16c185f5b63f6872f0fb64d0d92c61cd158bdd1
Christian Lorenzo <clbmiami2004@gmail.com>
2019-12-03 19:07:06 -0800

    Finished basic UI

---
+++ Tasks-CoreData/TaskDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TaskDetailViewController.swift
+//  Tasks-CoreData
+//
+//  Created by Lambda_School_Loaner_201 on 12/3/19.
+//  Copyright © 2019 Christian Lorenzo. All rights reserved.
+//
+
+import UIKit
+
+class TaskDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
